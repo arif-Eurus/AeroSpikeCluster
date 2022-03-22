0bd028a88fbf8991c3c1b8976465de0c2d8130c7
arif-Eurus <arif@eurustechnologies.com>
2022-03-22 15:16:48 -0700

    dev commit

---
+++ aerospike-delete-node.groovy
@@ -0,0 +1,70 @@
+def nodeip = ''
+def ec2_node_name=''
+pipeline {
+  agent any
+    parameters {
+            string(defaultValue: '', name: 'NODE_NAME', trim: true)
+        }
+    stages {
+      stage('Get Last Node of Cluster') {
+        steps {
+            script {
+              if(params.NODE_NAME.isEmpty()){
+                  try {
+                      ec2_node_name = sh script:"""#!/bin/bash
+                      source ./script/aerospike_delete_node.sh
+                      get_last_node_metadata
+                      """, returnStdout: true
+                      println "Agent info within script: ${ec2_node_name}"
+                  }
+                  catch (err) {
+                      currentBuild.result = 'FAILURE'
+                      emailExtraMsg = "Build Failure:"+ err.getMessage()
+                      throw err
+                  }
+              }
+              }
+          }
+        }
+      }
+
+    stage('Get Node of Cluster') {
+      steps {
+          script {
+            if(!params.NODE_NAME){
+                try {
+                    ec2_node_name = sh script:"""#!/bin/bash
+                    source ./script/aerospike_delete_node.sh
+                    get_node_name
+                    """, returnStdout: true
+                    println "Agent info within script: ${ec2_node_name}"
+                }
+                catch (err) {
+                    currentBuild.result = 'FAILURE'
+                    emailExtraMsg = "Build Failure:"+ err.getMessage()
+                    throw err
+                }
+            }
+            }
+        }
+      }
+    stage('Extract Last Node IP') {
+      steps {
+        script {
+            try {
+                nodeip = sh script:"""#!/bin/bash
+                    source ./script/aerospike_delete_node.sh
+                    extract_ip_of_last_node \$(echo "${ec2_node_name}")
+                    """, returnStdout: true
+                println "Agent info within script: ${nodeip}"
+            }
+            catch (err) {
+                currentBuild.result = 'FAILURE'
+                emailExtraMsg = "Build Failure:"+ err.getMessage()
+                throw err
+              }
+          }
+        }
+      }
+ 
+}
